bb6ac779b70c42b6a258b884820e43a32346d4a6
velinovasen <velinovasen@users.noreply.github.com>
2020-09-11 16:18:52 +0300

    'dsd'

--- tennis_scheduled.py
+++ tennis_scheduled.py
@@ -15,15 +15,19 @@ class TennisScheduled:
 
         # GET THE HTML
         options = FirefoxOptions()
-        options.headless = True
+        options.headless = False
         driver = Firefox(options=options, executable_path='C:\Windows\geckodriver.exe')
         driver.get(self.WEB_LINKS["tennis"])
-        time.sleep(2)
+        time.sleep(3)
+        driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[1]/div[2]/div/div[1]/div/div[1]/div[2]/div/div[1]/div").click()
+        elements = driver.find_elements_by_class_name("sr-tournament sr-collapsed")
+        [print(el) for el in elements]
+        time.sleep(15)
         html = driver.execute_script('return document.documentElement.outerHTML;')
 
         soup = BeautifulSoup(html, 'html.parser')
         games = soup.find_all(class_=re.compile("sr-tournament sr-collapsed"))
-        [print(game) for game in games]
+        [game.click() for game in games] # TO FINISH NOT WORKING !
         driver.close()
 
 
